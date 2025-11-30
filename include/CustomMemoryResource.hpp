b0238fb7342ac317122fe595bad3113b5d924566
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-30 22:01:59 +0300

    Replace "pointer" to "ptr"

--- include/CustomMemoryResource.hpp
+++ include/CustomMemoryResource.hpp
@@ -8,11 +8,11 @@ class CustomMemoryResource: public std::pmr::memory_resource {
   private:
     // блок в буфере
     struct block {
-      void *pointer_;  // указатель на начало
+      void *ptr_;  // указатель на начало
       size_t size_;    // размер
       bool is_free_;   // статус
 
-      block(void *pointer, size_t size, bool is_free = false): pointer_(pointer), size_(size), is_free_(is_free) {}
+      block(void *ptr, size_t size, bool is_free = false): ptr_(ptr), size_(size), is_free_(is_free) {}
     };
     
     void *memory_;             // указатель буфер
@@ -23,27 +23,27 @@ class CustomMemoryResource: public std::pmr::memory_resource {
     void mergeFreeBlocks();
 
     void insertFreeBlockBefore(auto iter, size_t aligned_gap) {
-      blocks_.insert(iter, block(iter->pointer_, aligned_gap, true));
+      blocks_.insert(iter, block(iter->ptr_, aligned_gap, true));
     }
 
-    void pushFreeBlockBefore(void *remaining_pointer, size_t aligned_gap) {
-      blocks_.push_back(block(remaining_pointer, aligned_gap, true));
+    void pushFreeBlockBefore(void *remaining_ptr, size_t aligned_gap) {
+      blocks_.push_back(block(remaining_ptr, aligned_gap, true));
     }
 
-    void updateCurrentBlock(auto iter, size_t bytes, void *aligned_pointer) {
-      iter->pointer_ = aligned_pointer;
+    void updateCurrentBlock(auto iter, size_t bytes, void *aligned_ptr) {
+      iter->ptr_ = aligned_ptr;
       iter->size_ = bytes;
       iter->is_free_ = false;
     }
 
-    void pushCurrentBlock(void *aligned_pointer, size_t bytes) {
-      blocks_.push_back(block(aligned_pointer, bytes, false));
+    void pushCurrentBlock(void *aligned_ptr, size_t bytes) {
+      blocks_.push_back(block(aligned_ptr, bytes, false));
     }
 
-    void insertFreeBlockAfter(auto iter, void *aligned_pointer, size_t bytes, size_t aligned_size) {
+    void insertFreeBlockAfter(auto iter, void *aligned_ptr, size_t bytes, size_t aligned_size) {
       const size_t remaining_size = aligned_size - bytes;
-      const void *remaining_pointer = static_cast<char *>(aligned_pointer) + bytes;
-      blocks_.insert(std::next(iter), block(remaining_pointer, remaining_size, true));
+      const void *remaining_ptr = static_cast<char *>(aligned_ptr) + bytes;
+      blocks_.insert(std::next(iter), block(remaining_ptr, remaining_size, true));
     }
 
   public:
@@ -56,44 +56,44 @@ class CustomMemoryResource: public std::pmr::memory_resource {
         throw std::bad_alloc();
       }
 
-      void *aligned_pointer;
+      void *aligned_ptr;
       size_t aligned_gap, aligned_size;
 
       for (auto iter = blocks_.begin(); iter != blocks_.end(); ++iter) {
         if (iter->is_free_ && iter->size_ >= bytes) {
-          aligned_pointer = std::align(alignment, bytes, iter->pointer_, iter->size_);
-          if (aligned_pointer) {
-            aligned_gap = static_cast<char *>(aligned_pointer) - static_cast<char *>(iter->pointer_);
+          aligned_ptr = std::align(alignment, bytes, iter->ptr_, iter->size_);
+          if (aligned_ptr) {
+            aligned_gap = static_cast<char *>(aligned_ptr) - static_cast<char *>(iter->ptr_);
             if (aligned_gap > 0) {
               insertFreeBlockBefore(iter, aligned_gap);
             }
             
-            updateCurrentBlock(iter, bytes, aligned_pointer);
+            updateCurrentBlock(iter, bytes, aligned_ptr);
 
             aligned_size = iter->size_ - aligned_gap;
             if (aligned_size > bytes) {
-              insertFreeBlockAfter(iter, aligned_pointer, bytes, aligned_size);
+              insertFreeBlockAfter(iter, aligned_ptr, bytes, aligned_size);
             }
 
-            return aligned_pointer;
+            return aligned_ptr;
           }
         }
       }
 
       size_t remaining_size = size_ - offset_;
       if (remaining_size >= bytes) {
-        void *remaining_pointer = static_cast<void *>(static_cast<char *>(memory_) + offset_);
-        aligned_pointer = std::align(alignment, bytes, remaining_pointer, remaining_size);
-        if (aligned_pointer) {
-          aligned_gap = static_cast<char *>(aligned_pointer) - static_cast<char *>(remaining_pointer);
+        void *remaining_ptr = static_cast<void *>(static_cast<char *>(memory_) + offset_);
+        aligned_ptr = std::align(alignment, bytes, remaining_ptr, remaining_size);
+        if (aligned_ptr) {
+          aligned_gap = static_cast<char *>(aligned_ptr) - static_cast<char *>(remaining_ptr);
           if (aligned_gap > 0) {
-            pushFreeBlockBefore(remaining_pointer, aligned_gap);
+            pushFreeBlockBefore(remaining_ptr, aligned_gap);
           }
 
-          pushCurrentBlock(aligned_pointer, bytes);
+          pushCurrentBlock(aligned_ptr, bytes);
 
           offset_ += aligned_gap + bytes;
-          return aligned_pointer;
+          return aligned_ptr;
         }
       }
       
@@ -101,7 +101,7 @@ class CustomMemoryResource: public std::pmr::memory_resource {
     }
 
     // Освобождение памяти
-    void do_deallocate(void *pointer, size_t bytes, size_t alignment) override;
+    void do_deallocate(void *ptr, size_t bytes, size_t alignment) override;
 
     // Сравнение с другим memory_resource
     bool do_is_equal(const std::pmr::memory_resource &other) const noexcept override;
