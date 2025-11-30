6cab51c94aa5df04e8e534b4275e6f05c9a72ce9
Ivan Babitskiy <ivan4ik.babi4ik777@yandex.ru>
2025-11-30 22:13:16 +0300

    Delete linear add of blocks

--- include/CustomMemoryResource.hpp
+++ include/CustomMemoryResource.hpp
@@ -17,7 +17,6 @@ class CustomMemoryResource: public std::pmr::memory_resource {
     
     void *memory_;             // указатель буфер
     size_t size_;              // размер буфера
-    size_t offset_ = 0;        // количество распределённых байтов
     std::list<block> blocks_;  // список блоков
   
     void mergeFreeBlocks();
@@ -26,18 +25,10 @@ class CustomMemoryResource: public std::pmr::memory_resource {
       blocks_.insert(iter, block(iter->ptr_, aligned_gap, true));
     }
 
-    void pushFreeBlockBefore(void *remaining_ptr, size_t aligned_gap) {
-      blocks_.push_back(block(remaining_ptr, aligned_gap, true));
-    }
-
     void updateCurrentBlock(auto iter, size_t bytes, void *aligned_ptr) {
       iter->ptr_ = aligned_ptr;
       iter->size_ = bytes;
       iter->is_free_ = false;
-    }
-
-    void pushCurrentBlock(void *aligned_ptr, size_t bytes) {
-      blocks_.push_back(block(aligned_ptr, bytes, false));
     }
 
     void insertFreeBlockAfter(auto iter, void *aligned_ptr, size_t bytes, size_t aligned_size) {
@@ -47,7 +38,11 @@ class CustomMemoryResource: public std::pmr::memory_resource {
     }
 
   public:
-    CustomMemoryResource(size_t size): size_(size), memory_(static_cast<void *>(new char[size])) {}
+    CustomMemoryResource(size_t size) {
+      size_ = size;
+      memory_ = static_cast<void *>(new char[size]);
+      blocks_.push_back(block(memory_, size, true));
+    }
 
     // Выделение памяти с заданным выравниванием
     void *do_allocate(size_t bytes, size_t alignment) override {
@@ -79,23 +74,6 @@ class CustomMemoryResource: public std::pmr::memory_resource {
 
             return aligned_ptr;
           }
-        }
-      }
-
-      size_t remaining_size = size_ - offset_;
-      if (remaining_size >= bytes) {
-        void *remaining_ptr = static_cast<void *>(static_cast<char *>(memory_) + offset_);
-        aligned_ptr = std::align(alignment, bytes, remaining_ptr, remaining_size);
-        if (aligned_ptr) {
-          aligned_gap = static_cast<char *>(aligned_ptr) - static_cast<char *>(remaining_ptr);
-          if (aligned_gap > 0) {
-            pushFreeBlockBefore(remaining_ptr, aligned_gap);
-          }
-
-          pushCurrentBlock(aligned_ptr, bytes);
-
-          offset_ += aligned_gap + bytes;
-          return aligned_ptr;
         }
       }
       
